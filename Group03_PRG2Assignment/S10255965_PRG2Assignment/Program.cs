4269c2eab50569322b6b0da22a34c4e06158652e
Chan-Man-Lock-Jake <jakechan.ml@gmail.com>
2024-01-11 13:19:54 +0800

    Update Program.cs

--- Group03_PRG2Assignment/S10255965_PRG2Assignment/Program.cs
+++ Group03_PRG2Assignment/S10255965_PRG2Assignment/Program.cs
@@ -61,6 +61,139 @@ void RegisterNewCustomer(Dictionary<int, Customer> customerDict, string filename
     }
 }
 
+IceCream CreateIceCream()
+{
+    //Create ice cream
+    IceCream? iceCream = null;
+
+    //Option
+    string[] o_menu = { "Cup", "Cone", "Waffle" };
+    string[] w_menu = { "Red velvet", "Charcoal", "Pandan" };
+    bool dipped = false;
+    string waffleFlavour = "";
+
+    Console.WriteLine("-Type-");
+
+    //print type options menu
+    Console.WriteLine("Avilable types:");
+    for (int i = 0; i < o_menu.Length; i++)
+    {
+        Console.WriteLine($"[{i + 1}] {o_menu[i]}");
+    }
+
+    Console.Write("\nEnter option : ");
+    int option = Convert.ToInt32(Console.ReadLine());
+
+    if (option == 2)
+    {
+        Console.Write("Add chocolate-dipped cone? y/n : ");
+        string dip = Console.ReadLine();
+        if (dip == "y") dipped = true;
+    }
+    else if (option == 3)
+    {
+        //print waffle flavour options menu
+        Console.WriteLine("Available waffle flavours: ");
+        for (int e = 0; e < o_menu.Length; e++)
+        {
+            Console.WriteLine($"[{e + 1}] {w_menu[e]}");
+        }
+        Console.Write("Select waffle flavour : ");
+        int waffF = Convert.ToInt32(Console.ReadLine());
+        waffleFlavour = w_menu[waffF - 1];
+    }
+
+    //Scoops
+    Console.Write("\n-Scoops-\nEnter number of scoops : ");
+    int scoops = Convert.ToInt16(Console.ReadLine());
+
+    //Flavours
+    string[] f_menu = { "Vanilla", "Chocolate", "Strawberry", "Durian", "Ube", "Sea salt" };
+    List<Flavour> f_list = new List<Flavour>();
+    string f_type = "";
+    bool premium = false;
+    int quantity = 1;
+
+    Console.Write("\n-Flavours-");
+
+    for (int i = 0; i < scoops;)
+    {
+        //print flavour options menu
+        Console.WriteLine("\nAvailable Flavours: ");
+        for (int e = 0; e < f_menu.Length; e++)
+        {
+            if (e == 0) Console.WriteLine("Ordinary");
+            else if (e == 3) Console.WriteLine("Premium");
+            Console.WriteLine($"[{e + 1}] {f_menu[e]}");
+        }
+
+        Console.Write("\nEnter flavour option : ");
+        int f_opt = Convert.ToInt16(Console.ReadLine());
+        if (f_opt >= 3 && f_opt <= 6) premium = true;
+        if (scoops == 1)
+        {
+            Flavour flavour = new Flavour(f_type, premium, quantity);
+            break;
+        }
+        else
+        {
+            Console.Write("Enter scoops of flavour : ");
+            quantity = Convert.ToInt16(Console.ReadLine());
+            Flavour f_add = new Flavour(f_type, premium, quantity);
+            i += quantity;
+        }
+    }
+
+    //Toppings
+    string[] t_menu = { "Sprinkles", "Mochi", "Sago", "Oreos" };
+    List<Topping> t_list = new List<Topping>();
+    string t_type = "";
+
+    Console.WriteLine("\n-Toppings-");
+
+    while (true)
+    {
+        if (t_list.Count == 4)
+        {
+            Console.WriteLine("Topping limit reached.");
+            break;
+        }
+
+        Console.Write("Add toppings? y/n : ");
+        string t_cont = Console.ReadLine();
+        if (t_cont == "n") break;
+        else if (t_cont == "y")
+        {
+            //print topping options menu
+            Console.WriteLine("\nAvailable Toppings:");
+            for (int i = 0; i < t_menu.Length; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {t_menu[i]}");
+            }
+
+            Console.Write("\nEnter topping option : ");
+            int t_opt = Convert.ToInt16(Console.ReadLine());
+            Topping topping = new Topping(t_menu[t_opt - 1]);
+            t_list.Add(topping);
+        }
+    }
+
+    switch (o_menu[option - 1])
+    {
+        case "Cup":
+            iceCream = new Cup("Cup", scoops, f_list, t_list);
+            break;
+        case "Cone":
+            iceCream = new Cone("Cone", scoops, f_list, t_list, dipped);
+            break;
+        case "Waffle":
+            iceCream = new Waffle("Waffle", scoops, f_list, t_list, waffleFlavour);
+            break;
+    }
+
+    return iceCream;
+}
+
 void CreateCustomerOrder(Dictionary<int, Customer> customerDict)
 {
     //Retrieve the selected customer
@@ -76,124 +209,9 @@ void CreateCustomerOrder(Dictionary<int, Customer> customerDict)
     customerDict[opt].CurrentOrder = new_order;
 
     //Create ice cream
-    IceCream new_icecream = new Cup();
+    IceCream iceCream = CreateIceCream();
 
-    Console.WriteLine("-Type-");
-    while (true)
-    {
-        //Option
-        string[] o_menu = { "Cup", "Cone", "Waffle" };
-        string[] w_menu = { "Red velvet", "Charcoal", "Pandan" };
-        bool dipped = false;
-        string waffleFlavour = "";
-
-        Console.WriteLine("Avilable types:");
-        for (int i = 0; i < o_menu.Length; i++)
-        {
-            Console.WriteLine($"[{i + 1}] {o_menu[i]}");
-        }
-
-        Console.Write("\n-Type-\nEnter option : ");
-        int option = Convert.ToInt32(Console.ReadLine());
-        new_icecream.Option = o_menu[option - 1];
-
-        if (option == 2)
-        {
-            Console.Write("Add chocolate-dipped cone? y/n : ");
-            string dip = Console.ReadLine();
-            if (dip == "y") dipped = true;
-        }
-        else if (option == 3)
-        {
-            Console.WriteLine("Available waffle flavours: ");
-            for (int e = 0; e < o_menu.Length; e++)
-            {
-                Console.WriteLine($"[{e + 1}] {w_menu[e]}");
-            }
-            Console.Write("Select waffle flavour : ");
-            int waffF = Convert.ToInt32(Console.ReadLine());
-            waffleFlavour = w_menu[waffF - 1];
-        }
-
-        //Scoops
-        Console.Write("\n-Scoops-\nEnter number of scoops : ");
-        int scoops = Convert.ToInt16(Console.ReadLine());
-        new_icecream.Scoops = scoops;
-
-        //Flavours
-        string[] f_menu = { "Vanilla", "Chocolate", "Strawberry", "Durian", "Ube", "Sea salt" };
-        List<Flavour> f_list = new List<Flavour>();
-        string f_type = "";
-        bool premium = false;
-        int quantity = 1;
-
-        Console.Write("\n-Flavours-");
-
-        for (int i = 0; i < scoops;)
-        {
-            Console.WriteLine("\nAvailable Flavours: ");
-            for (int e = 0; e < f_menu.Length; e++)
-            {
-                if (e == 0) Console.WriteLine("Ordinary");
-                else if (e == 3) Console.WriteLine("Premium");
-                Console.WriteLine($"[{e + 1}] {f_menu[e]}");
-            }
-
-            Console.Write("\nEnter flavour option : ");
-            int f_opt = Convert.ToInt16(Console.ReadLine());
-            if (f_opt >= 3 && f_opt <= 6) premium = true;
-            if (scoops == 1)
-            {
-                Flavour flavour = new Flavour(f_type, premium, quantity);
-                new_icecream.Flavours.Add(flavour);
-                break;
-            }
-            else
-            {
-                Console.Write("Enter scoops of flavour : ");
-                quantity = Convert.ToInt16(Console.ReadLine());
-                Flavour f_add = new Flavour(f_type, premium, quantity);
-                new_icecream.Flavours.Add(f_add);
-                i += quantity;
-            }
-        }
-
-        //Toppings
-        string[] t_menu = { "Sprinkles", "Mochi", "Sago", "Oreos" };
-        List<Topping> t_list = new List<Topping>();
-        string t_type = "";
-
-        Console.WriteLine("\n-Toppings-");
-
-        while (true)
-        {
-            if (t_list.Count == 4)
-            {
-                Console.WriteLine("Topping limit reached.");
-                break;
-            }
-
-            Console.Write("Add toppings? y/n : ");
-            string t_cont = Console.ReadLine();
-            if (t_cont == "n") break;
-            else if (t_cont == "y")
-            {
-                //print menu
-                Console.WriteLine("\nAvailable Toppings:");
-                for (int i = 0; i < t_menu.Length; i++)
-                {
-                    Console.WriteLine($"[{i + 1}] {t_menu[i]}");
-                }
-
-                Console.Write("\nEnter topping option : ");
-                int t_opt = Convert.ToInt16(Console.ReadLine());
-                Topping topping = new Topping(t_menu[t_opt - 1]);
-                t_list.Add(topping);
-                new_icecream.Toppings.Add(topping);
-            }
-        }
-        Console.WriteLine(new_icecream);
-    }
+    Console.WriteLine(iceCream);
 }
 
 ExtractCustomer(customerFile);
