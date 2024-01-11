f1b5c6fc1211ee638807d0d582c0ead1a1c74c62
Koyonari <yongshyna.an@gmail.com>
2024-01-11 09:29:38 +0800

    Update: Option 4

--- Group03_PRG2Assignment/S10258126_PRG2Assignment/Program.cs
+++ Group03_PRG2Assignment/S10258126_PRG2Assignment/Program.cs
@@ -26,7 +26,7 @@ void DisplayMenu()
 //1. List all customers
 void ListCustomers()
 {
-
+    Console.WriteLine();
 }
 
 //3. Register a new customer
@@ -34,8 +34,7 @@ void RegisterCustomer()
 {
     //Prompt user for info
     Console.Write("Please enter customers information (name, id number, date of birth): ");
-    string customer_info = Console.ReadLine();
-    string[] customer_data = customer_info.Split(",");
+    string[] customer_data = Console.ReadLine().Split(",");
 
     //Create customer object with info
     if (customer_data.Length != 3)
@@ -62,12 +61,69 @@ void RegisterCustomer()
         //Display message to indicate registration status
         Console.WriteLine("Customer registered succesfully!");
     }
+    Console.WriteLine();
 }
 
 //4. Create a customer's order
 void CreateOrder()
 {
+    //List customers name from csv file
+    string[] data = File.ReadAllLines(path);
+    List<string> customer_names = data.ToList();
+    customer_names.RemoveAt(0);
+    foreach (string line in customer_names)
+    {
+        string[] customer_data = line.Split(",");
+        Console.WriteLine(customer_data[0]);
+    }
 
+    //Prompt user to select a customer and retrieve selected
+    Console.Write("Please select a customer: ");
+    string customer_name = Console.ReadLine();
+
+    bool found = false;
+    foreach (string customer in customer_names)
+    {
+        string[] customer_data = customer.Split(",");
+
+        if (customer_name.ToLower() == customer_data[0].ToLower())
+        {
+            found = true;
+            Console.WriteLine($"Customer found: {customer_data[0]}");
+            break;
+        }
+    }
+    if (found == false)
+    {
+        Console.WriteLine("Customer not found. Please try again");
+    }
+
+    //Create order object
+    Order neworder = new Order();
+
+    string add_another = "";
+    do
+    {
+        //Prompt user to enter ice cream details
+        Console.Write("Please enter ice cream details (option, scoops, flavours, toppings): ");
+
+        //Create ice cream object
+        string[] icecream_details = Console.ReadLine().Split(",");
+
+        string option = icecream_details[0];
+        int scoops = int.Parse(icecream_details[1]);
+        List<Flavour> flavours = new List<Flavour>();
+        string[] type_premium_quantity = icecream_details[2].Split(" ");
+        flavours.Add(new Flavour(type_premium_quantity[0], bool.Parse(type_premium_quantity[1]), int.Parse(type_premium_quantity[2])));
+        List<Topping> toppings = new List<Topping>();
+        toppings.Add(new Topping(icecream_details[3]));
+
+        //Prompt user to add another ice cream
+        Console.Write("Add another ice cream? (Y/N): ");
+        add_another = Console.ReadLine();
+    } while (add_another.ToLower() == "y" || add_another.ToLower() == "yes");
+
+    Console.WriteLine();
 }
 
 //Use while loop to keep calling menu method until exited
